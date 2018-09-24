15548d511f15c2398aab29599617c75cee68d8b7
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-24 12:17:48 -0500

    Finish 6ees6ees6ees Repo

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(age, color, food, job) {
+    super(age, color, food, job);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    this.honeyPot++;
+  }
+
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,17 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee{
+  constructor (age, job, color, food) {
+    super();
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead'
+  }
+
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
